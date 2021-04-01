88c2cf8bcc7f1743eccd8aab24a2cb14318fd956
Christian Bouwense <christian.bouwense@ingka.ikea.com>
2021-04-01 14:38:00 -0400

    added CoD checking functionality

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+import express from "express";
+import bp from "body-parser";
+import fetch from "node-fetch";
+
 import { Kafka } from "kafkajs";
+
+const app = express();
+
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
+
+const res = await fetch("https://www.ikea.com/us/en/data-sources/4c852290032c11eb995be32ed4ecf18b.json");
+const codList = (await res.json()).products;
 
 const kafka = new Kafka({
     clientId: "my-app",
@@ -6,18 +18,28 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-
 console.log("Producer connecting...");
 await producer.connect();
 console.log("Producer connected successfully!");
-await producer.send({
-    topic: "test-topic",
-    messages: [
-        { value: "New message" },
-    ],
+
+app.post("/", async (req, res) => {
+    try {
+        console.log(req.body.topic)
+        await producer.send({
+            topic: req.body.topic,
+            messages: [
+                { value: JSON.stringify(req.body.message) },
+            ],
+        });
+        res.status(200).send("Message sent successfully");
+    } catch (err) {
+        res.status(500).send(`Message sent unsuccessfully: ${err}`);
+    }
 });
 
-await producer.disconnect();
+app.listen(3000, () => {
+    console.log("Express server running on port 3000");
+});
 
 const consumer = kafka.consumer({ groupId: "test-group" });
 
@@ -34,13 +56,26 @@ console.log("Consumer subscribed successfully!");
 console.log("Consumer awaiting messages...");
 await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
+        console.log("message: ", message)
+        const sku = JSON.parse(message.value).sku.toString();
         console.log("\n==============");
         console.log("topic: ", topic);
         console.log("partition: ", partition);
         console.log({
-            value: message.value.toString(),
+            value: sku,
         });
-        console.log("==============\n");
+        console.log("==============");
+
+        console.log("Checking if product is CoD item...");
+        try {
+            if (Object.keys(codList).includes(sku)) {
+                console.log(`*** ${sku} IS A COD ITEM ***`);
+            } else {
+                console.log(`*** ${sku } IS NOT A COD ITEM`);
+            }
+        } catch (err) {
+            console.log(err);
+        }
     },
 });
 console.log("Consumer read messages successfully!");
